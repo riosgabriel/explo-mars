36bb222ff4b01b45d725e38e01c7ec79f65df46c
Gabriel Rios <gabriel.rios@99taxis.com>
2017-08-30 11:15:27 -0300

    Hover functions

---
+++ lib/hover.ex
@@ -0,0 +1,32 @@
+defmodule Hover do
+
+  defstruct x: 0, y: 0, direction: "N"
+
+  def rotate(hover, "L") do
+    case hover.direction do
+      "N" -> %{hover | direction: "W"}
+      "W" -> %{hover | direction: "S"}
+      "E" -> %{hover | direction: "N"}
+      "S" -> %{hover | direction: "E"}
+    end
+  end
+
+  def rotate(hover, "R") do
+    case hover.direction do
+      "N" -> %{hover | direction: "E"}
+      "W" -> %{hover | direction: "N"}
+      "E" -> %{hover | direction: "S"}
+      "S" -> %{hover | direction: "W"}
+    end
+  end
+
+  def move(hover) do
+    case hover.direction do
+      "N" -> %{hover | y: hover.y + 1}
+      "W" -> %{hover | x: hover.x - 1}
+      "E" -> %{hover | x: hover.x + 1}
+      "S" -> %{hover | y: hover.y - 1}
+    end
+  end
+
+end
